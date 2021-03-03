085b345567c1891aabd57c333bbcb682986b5758
Mohammed Sohail <itscrazybob@gmail.com>
2021-03-03 13:35:49 +0400

    wip

--- src/Shape2D.java
+++ src/Shape2D.java
@@ -1,3 +1,12 @@
+/**
+ * Lab03 Instructions for Lab03 are in CS102_Lab03.pdf file located in the root
+ * directory of Lab03 Revisions can be seen on the following GitHub URL:
+ * https://github.com/thecrazybob/CS102-lab03 Style Guidelines:
+ * http://www.cs.bilkent.edu.tr/~adayanik/cs101/practicalwork/styleguidelines.htm
+ *
+ * @author Mohammed Sohail
+ * @version 03/03/2021
+ */
 
 public abstract class Shape2D {
 
